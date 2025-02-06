1a03df60e283c21ad16ea0d7ce1d5d4f7fcc6fa6
Thomas_Tan <thomas.tan@riversoft.com.tw>
2025-02-06 10:01:07 +0800

    use Ollama to do G in RAG

--- llm.py
+++ llm.py
@@ -1,3 +1,16 @@
-# QA
+import os
+os.environ["OLLAMA_HOST"] = "http://192.168.1.221:11434"
+from langchain_ollama import ChatOllama
 
-# Query
+llm = ChatOllama(model="llama3.3-70B")
+# QA
+def qa(query: str):
+    rag_options: list[str] = [""]
+    results = llm.invoke(f"Judging from the user query '{query}', what is its most suitable response in the following options: '{rag_options}'.")
+    return results.content
+
+# Paraphrase
+def paraphrase(query: str):
+    rag_options: list[str] = [""]
+    results = llm.invoke(f"Judging from the user query '{query}', what is its most similar response in the following options: '{rag_options}'.")
+    return results.content
